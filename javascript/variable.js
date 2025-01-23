7a412faf6fb27c32b220eb152e6fe35a01afb55b
Sreya410 <sreya3492@gmail.com>
2025-01-23 19:24:51 +0530

    sreya

---
+++ javascript/variable.js
@@ -0,0 +1,15 @@
+var a;
+console.log(typeof a)
+let b;
+b=20
+console.log(typeof b)
+const c="Raja";
+console.log(typeof c)
+console.log(b-c)
+a=null
+console.log(typeof a)
+var obj={
+    fname:"Raja",
+    lname:"m",
+}
+console.log(typeof obj)
